f4b06b1cabd3b08d78bd68f4d7ae20294923ac2a
braoru <braoru@gmail.com>
2012-10-24 20:19:58 +0200

    Cleaning

--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,19 @@ object ApplicationBuild extends Build {
     val appName         = "play-template"
     val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
+    val appDependencies = Seq(   
+        "com.typesafe" % "slick_2.10.0-M7" % "0.11.1",
+        "net.sourceforge.jtds" % "jtds" % "1.2",
+        "org.squeryl" %% "squeryl" % "0.9.5-2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+	 resolvers := Seq(
+      "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
+      "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases",
+      "sonatype-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "java-net" at "http://download.java.net/maven/2",
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/")
+	)
 
 }
